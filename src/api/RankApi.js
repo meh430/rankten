1816f280ea882553caf4592fb8a62b1841bee13d
Mehul Pillai <mehulpillai30@gmail.com>
2020-09-05 10:59:56 -0400

    Implements relevant request functions

--- src/api/RankApi.js
+++ src/api/RankApi.js
@@ -1,13 +1,13 @@
-const baseUrl = 'http://192.168.0.22:5000';
+const baseUrl = "http://192.168.0.22:5000";
 
 async function validateImage(imageUrl) {
     var isValid = false;
     try {
         var response = await fetch(imageUrl);
-        isValid = response.ok && response.headers['Content-Type'].includes('image');
+        isValid = response.ok && response.headers["Content-Type"].includes("image");
     } catch (e) {
         return false;
-    } 
+    }
 
     return isValid;
 }
@@ -17,11 +17,109 @@ async function get(endpoint, bearerToken = "") {
     var jsonResponse;
 
     try {
-        var response = await fetch(baseUrl + endpoint, );
+        var jsonResponse = parseResponse(await fetch(baseUrl + endpoint, {
+            method: 'GET',
+            headers: getHeaders(bearerToken)
+        }));
     } catch (e) {
         hasError = true;
         jsonResponse = e;
     }
 
     return [hasError, jsonResponse];
+}
+
+async function post(endpoint, body, bearerToken = "") {
+    if (!body) {
+        body = {};
+    }
+
+    var hasError = false;
+    var jsonResponse;
+
+    try {
+        var jsonResponse = parseResponse(await fetch(baseUrl + endpoint, {
+            method: 'POST',
+            body: JSON.stringify(body),
+            headers: getHeaders(bearerToken)
+        }));
+    } catch (e) {
+        hasError = true;
+        jsonResponse = e;
+    }
+
+    return [hasError, jsonResponse];
+}
+
+async function put(endpoint, body, bearerToken = "") {
+    if (!body) {
+        body = {};
+    }
+
+    var hasError = false;
+    var jsonResponse;
+
+    try {
+        var jsonResponse = parseResponse(await fetch(baseUrl + endpoint, {
+            method: 'PUT',
+            body: JSON.stringify(body),
+            headers: getHeaders(bearerToken)
+        }));
+    } catch (e) {
+        hasError = true;
+        jsonResponse = e;
+    }
+
+    return [hasError, jsonResponse];
+}
+
+async function del(endpoint, bearerToken = "") {
+    var hasError = false;
+    var jsonResponse;
+
+    try {
+        var jsonResponse = parseResponse(await fetch(baseUrl + endpoint, {
+            method: 'DELETE',
+            headers: getHeaders(bearerToken)
+        }));
+    } catch (e) {
+        hasError = true;
+        jsonResponse = e;
+    }
+
+    return [hasError, jsonResponse];
+}
+
+function getHeaders(bearerToken = "") {
+    return bearerToken == "" ?
+        {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+        } :
+        {
+            Authorization: "Bearer $bearerToken",
+            Accept: "application/json",
+            "Content-Type": "application/json",
+        };
+}
+
+function parseResponse(response) {
+    switch (response.status) {
+        case 200:
+            var parsed = await response.json();
+            return parsed;
+        case 400:
+            var parsed = await response.json();
+            if (parsed.message.includes("page")) {
+                throw "Page does not exist";
+            } else {
+                throw "Bad request";
+            }
+            case 401:
+                throw "Authentication error";
+            case 403:
+                throw "Unauthorized";
+            default:
+                throw "Something went wrong";
+    }
 }
