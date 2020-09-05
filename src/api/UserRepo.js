f2f66a55e40370d5b3f8eb44541169531ced0683
Mehul Pillai <mehulpillai30@gmail.com>
2020-09-05 12:30:52 -0400

    Implements follow in repo

--- src/api/UserRepo.js
+++ src/api/UserRepo.js
@@ -14,3 +14,16 @@ export async function updateProfilePic(profPic, token) {
     var response = await api.put('/users', token, { 'prof_pic': profPic });
     return response;
 }
+
+export async function followUser(name, token) {
+    var [hasError, response] = await api.post('/follow/' + name, token);
+    if (hasError) {
+        return [hasError, response];
+    } else {
+        if (response.message.includes("unfollow")) {
+            return [true, "UNFOLLOW"];
+        } else {
+            return [true, "FOLLOW"];
+        }
+    }
+}
