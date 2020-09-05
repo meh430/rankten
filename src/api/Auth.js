9dabc81a60cfa7b4392d8804ff88b16326209fbf
Mehul Pillai <mehulpillai30@gmail.com>
2020-09-05 12:17:56 -0400

    Changes api returns

--- src/api/Auth.js
+++ src/api/Auth.js
@@ -2,26 +2,26 @@ import * as api from './RankApi';
 import { saveCred } from '../misc/PrefStore';
 
 export async function tokenValid(token) {
-    var [hasError, response] = await api.post('/validate_token', {}, token);
-    return hasError ? undefined : response;
+    var response = await api.post('/validate_token', {}, token);
+    return response;
 }
 
 export async function loginUser(userName, password) {
     var [hasError, response] = await api.post('/login', { 'user_name': userName, 'password': password });
     if (hasError) {
-        return undefined
+        return [hasError, response]
     } else {
         saveCred(response.jwtToken, userName, password);
-        return response;
+        return [hasError, response];
     }
 }
 
 export async function signupUser(userName, password, bio) {
     var [hasError, response] = await api.post('/signup', { 'user_name': userName, 'password': password, 'bio': bio });
     if (hasError) {
-        return undefined;
+        return [hasError, response];
     } else {
         saveCred(response.jwtToken, userName, password);
-        return response;
+        return [hasError, response];
     }
 }
--- src/api/UserRepo.js
+++ src/api/UserRepo.js
@@ -1,7 +1,12 @@
 import * as api from './RankApi';
 
 export async function getUser(name) {
-    var [hasError, response] = await api.get('/users/' + name);
-    return hasError ? undefined : response;
+    var response = await api.get('/users/' + name);
+    return response;
+}
+
+export async function updateBio(bio, token) {
+    var response = await api.put('/users', { 'bio': bio }, token)
+    return response;
 }
 
