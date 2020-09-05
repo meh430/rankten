c0b53ca4ed06da152c2a9d67a61e7952583cd83a
Mehul Pillai <mehulpillai30@gmail.com>
2020-09-05 11:12:18 -0400

    Implements store

--- src/misc/PrefStore.js
+++ src/misc/PrefStore.js
@@ -7,3 +7,25 @@ function saveDarkTheme(value) {
     localStorage.setItem(DARK_THEME, value);
 }
 
+function saveCred(token, userName, password) {
+    localStorage.setItem(JWT_TOKEN, token);
+    localStorage.setItem(USER_NAME_KEY, userName);
+    localStorage.setItem(PASSWORD_KEY, password);
+}
+
+function isDark() {
+    var dark = localStorage.getItem(DARK_THEME);
+    return dark != undefined ? dark : false;
+}
+
+function getToken() {
+    var token = localStorage.getItem(JWT_TOKEN);
+    return token;
+}
+
+function getLogin() {
+    var userName = localStorage.getItem(USER_NAME_KEY);
+    var password = localStorage.getItem(PASSWORD_KEY);
+
+    return [userName, password];
+}
