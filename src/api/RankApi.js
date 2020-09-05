187ec800e6f13a1678e56b85783a62203eeb3100
Mehul Pillai <mehulpillai30@gmail.com>
2020-09-05 10:51:31 -0400

    Adds more to api

--- src/api/RankApi.js
+++ src/api/RankApi.js
@@ -2,7 +2,14 @@ const baseUrl = 'http://192.168.0.22:5000';
 
 async function validateImage(imageUrl) {
     var isValid = false;
+    try {
+        var response = await fetch(imageUrl);
+        isValid = response.ok && response.headers['Content-Type'].includes('image');
+    } catch (e) {
+        return false;
+    } 
 
+    return isValid;
 }
 
 async function get(endpoint, bearerToken = "") {
