4a0862da6d724671c811e8ddc2a7fdc98c343cc8
Mehul Pillai <mehulpillai30@gmail.com>
2020-09-06 18:48:58 -0400

    simple validation on login

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,11 +1,19 @@
 import { Button, Card, CardContent, useTheme, TextField } from "@material-ui/core";
-import React from "react";
+import React, { useState } from "react";
 import { Link, Redirect } from "react-router-dom";
 import { appThemeConstants } from "../misc/AppTheme";
 import "../App.css";
+
+export const namePattern = new RegExp("^[a-z0-9_-]{3,15}$");
+export const passwordPattern = new RegExp("^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,}$");
+let userName = "";
+let password = "";
 export const Login = () => {
+
+
     const currentTheme = useTheme();
-    const [loginError, setError] = useState(false);
+    const [nameError, setNameError] = useState(false);
+    const [passwordError, setPasswordError] = useState(false);
     const [successfulLogin, setSuccess] = useState(false);
     const fieldTheme = {
         color: appThemeConstants.lavender,
@@ -19,9 +27,33 @@ export const Login = () => {
         color: currentTheme.palette.secondary.dark,
     };
 
-    const submitLogin = () => {};
+    const submitLogin = () => {
+        let error = false;
+        console.log(userName);
+        if (!userName.match(namePattern)) {
+            error = true;
+            setNameError(true);
+            setSuccess(false);
+        } else {
+            setNameError(false);
+        }
+        
+        if (!password.match(passwordPattern)) {
+            error = true;
+            setPasswordError(true);
+            setSuccess(false);
+        } else {
+            setPasswordError(false);
+        }
 
-    if (!loginError && successfulLogin) {
+        if(!error) {
+            //make api call to login. If has error, show snackbar, else push main route
+            setSuccess(true);
+        }
+
+    };
+
+    if (!passwordError && !nameError && successfulLogin) {
         return <Redirect to="/main" />;
     }
 
@@ -37,18 +69,31 @@ export const Login = () => {
         >
             <CardContent className="col" style={{ alignItems: "center", paddingBottom: "2px" }}>
                 <h1 style={textTheme}>Log In</h1>
-                <TextField style={fieldTheme} id="outlined-basic" label="Username" variant="outlined" />
+                <TextField
+                    error={nameError}
+                    helperText={nameError ? "Invalid username" : ""}
+                    style={fieldTheme}
+                    id="outlined-basic"
+                    label="Username"
+                    variant="outlined"
+                    onChange={(event) => (userName = event.target.value)}
+                />
 
                 <TextField
+                    error={passwordError}
+                    helperText={passwordError ? "Invalid password" : ""}
                     style={fieldTheme}
+
                     id="outlined-password-input"
                     label="Password"
                     type="password"
                     autoComplete="current-password"
                     variant="outlined"
+                    onChange={(event) => (password = event.target.value)}
                 />
 
                 <Button
+                    onClick={() => submitLogin()}
                     variant="contained"
                     style={{
                         width: "70%",
