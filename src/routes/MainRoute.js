442a82ac4507626dbc0efe976d5ff1869eee6ec0
Mehul Pillai <mehulpillai30@gmail.com>
2020-09-10 18:13:23 -0400

    tries to make bar responsive

--- src/routes/MainRoute.js
+++ src/routes/MainRoute.js
@@ -1,36 +1,48 @@
 import React, { useContext } from "react";
-import {Switch, Route, Redirect} from 'react-router-dom'
+import { Switch, Route, Redirect } from "react-router-dom";
 import { UserContext } from "../Contexts";
-import { AppBar, Toolbar, IconButton, TextField, InputAdornment, useTheme } from "@material-ui/core";
+import { AppBar, Toolbar, IconButton, TextField, Typography, useTheme } from "@material-ui/core";
 import MenuIcon from "@material-ui/icons/Menu";
 import SearchIcon from "@material-ui/icons/Search";
 import "../App.css";
+import { appThemeConstants } from "../misc/AppTheme";
 export const MainRoute = () => {
     const currentTheme = useTheme();
     //const { user } = useContext(UserContext);
     return (
-            <div style={{height: "100vh", backgroundColor: currentTheme.palette.background.default}} >
-            <AppBar position="static" style={{backgroundColor: currentTheme.palette.background.default, boxShadow: currentTheme.shadows[0], width: "100vw"}}>
+        <div style={{ height: "100vh", width:"100vw", backgroundColor: currentTheme.palette.background.default }}>
+            <AppBar
+                position="static"
+                style={{
+                    backgroundColor: currentTheme.palette.background.default,
+                    boxShadow: currentTheme.shadows[0],
+                    width: "100vw",
+                }}
+            >
                 <Toolbar>
                     <IconButton edge="start" color="inherit" aria-label="menu">
-                        <MenuIcon style={{color: currentTheme.palette.text.primary}}/>
+                        <MenuIcon style={{ color: currentTheme.palette.text.primary }} />
                     </IconButton>
-                    <div style={{width: "fit-content", maxWidth: "90%", flexGrow: "1"}}>
+                    <Typography
+                        style={{ fontFamily: appThemeConstants.fontFamily, color: currentTheme.palette.secondary.dark, marginLeft: "10px" }}
+                    >
+                        Rank 10
+                    </Typography>
+                    <div style={{ width: "fit-content", maxWidth: "80%" }}>
                         <TextField
-                            margin="dense"
-                            style={{width: "550px", margin: "8px", maxWidth: "100%"}}
+                            style={{ width: "550px", margin: "8px", maxWidth: "60%" }}
                             variant="outlined"
-                            placeholder="Search..." 
+                            placeholder="Search..."
                         />
                     </div>
                 </Toolbar>
-            </AppBar>   
+            </AppBar>
             <div>
                 <Switch>
-                    <Route path="/main" component={() => <h1>Feed</h1>} exact/>
-                    <Route path="/main/discover" component={() => <h1>Discover</h1>}/>
-                    <Route path="/main/search" component={() => <h1>Search</h1>}/>
-                    <Route path="/main/profile" component={() => <h1>Profile</h1>}/>
+                    <Route path="/main" component={() => <h1>Feed</h1>} exact />
+                    <Route path="/main/discover" component={() => <h1>Discover</h1>} />
+                    <Route path="/main/search" component={() => <h1>Search</h1>} />
+                    <Route path="/main/profile" component={() => <h1>Profile</h1>} />
                 </Switch>
             </div>
         </div>
