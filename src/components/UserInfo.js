9ef64a32030516a1ebfe0e4348b7cba9ebe6542b
Mehul Pillai <mehulpillai30@gmail.com>
2020-09-13 12:34:37 -0400

    Adds user info card

--- src/components/UserInfo.js
+++ src/components/UserInfo.js
@@ -22,22 +22,63 @@ export const UserInfo = (props) => {
     const classes = useStyles();
     const currentTheme = useTheme();
     return (
-        <Card style={{
+        <Card
+            style={{
                 margin: "10px",
                 alignSelf: "center",
                 boxShadow: currentTheme.shadows[4],
                 width: "800px",
                 maxWidth: "98%",
                 borderRadius: "10px",
-            }}>
-            <CardContent className="row">
+            }}
+        >
+            <CardContent className="row" style={{justifyContent: "space-evenly", alignItems: "center"}}>
                 <Avatar src="" className={classes.avatar}>
                     <AccountCircleIcon className={classes.avIcon} />
                 </Avatar>
                 <div className="row" style={{ flexWrap: "wrap", justifyContent: "space-evenly" }}>
-                    
+                    {
+                        [
+                            {
+                                stat: 12,
+                                label: "Rank Points"
+                            },
+                            {
+                                stat: 12,
+                                label: "Rank Lists"
+                            },
+                            {
+                                stat: 12,
+                                label: "Followers"
+                            },
+                            {
+                                stat: 12,
+                                label: "Following"
+                            },
+                            {
+                                stat: 12,
+                                label: "Comments"
+                            },
+                            {
+                                stat: 12,
+                                label: "Liked Lists"
+                            }
+                        ].map(userStat => <UserStat stat={userStat.stat} label={userStat.label} key={userStat.label}/>)
+                    }
                 </div>
             </CardContent>
         </Card>
+    );
+};
+
+//onClick: callback
+//label: string
+//stat: number
+const UserStat = (props) => {
+    return (
+        <div className="col" style={{ alignItems: "center", justifyContent: "center", margin: "10px" }}>
+            <h4>{props.stat}</h4>
+            <h5>{props.label}</h5>
+        </div>
     );
 };
