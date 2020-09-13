ea331f420d0f950f288b4cad2f3bd0d0654309bb
Mehul Pillai <mehulpillai30@gmail.com>
2020-09-13 13:02:10 -0400

    convert card style to a function

--- src/misc/AppTheme.js
+++ src/misc/AppTheme.js
@@ -1,15 +1,24 @@
 export const appThemeConstants = {
-    palePurple: '#f1e3f3',
-    lavender: '#c2bbf0',
-    darkSienna: '#4a051c',
-    hanPurple: '#6320ee',
-    paraPink: '#ef476f',
-    fontFamily: "'Nunito', sans-serif"
-}
+    palePurple: "#f1e3f3",
+    lavender: "#c2bbf0",
+    darkSienna: "#4a051c",
+    hanPurple: "#6320ee",
+    paraPink: "#ef476f",
+    fontFamily: "'Nunito', sans-serif",
+};
 
 export const getTextTheme = (theme) => {
     return {
         fontFamily: appThemeConstants.fontFamily,
-        color: theme.palette.secondary.dark
+        color: theme.palette.secondary.dark,
+    };
+};
+
+export const getCardStyle = (theme) => {
+    return {
+        margin: "10px",
+        alignSelf: "center",
+        boxShadow: currentTheme.shadows[4],
+        borderRadius: "10px",
     };
 };
