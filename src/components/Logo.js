8d095f17cdf15961bed8227d9da4a0c5f6954bcf
Mehul Pillai <mehulpillai30@gmail.com>
2020-09-18 20:52:27 -0400

    Adds alt prop to logo

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -5,6 +5,7 @@ export const Logo = (props) => {
     return (
         <img
             src={require("../assets/ranktenlogo.png")}
+            alt="Rank Ten logo"
             style={{ maxWidth: "80%", alignSelf: "center", paddingTop: "10px" }}
             width={`${props.width}px`}
         />
