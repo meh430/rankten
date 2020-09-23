d44a1d4614740c7ba976b79b011b138158d0c0a0
Mehul Pillai <mehulpillai30@gmail.com>
2020-09-23 16:34:32 -0400

    Sets up preview

---
+++ src/api/UserPreviewRepo.js
@@ -0,0 +1,17 @@
+import * as api from './RankApi';
+
+export async function getFollowing(params) {
+
+}
+
+export async function getFollowers(params) {
+
+}
+
+export async function getLikers(params) {
+
+}
+
+export async function searchUsers(params) {
+
+}
