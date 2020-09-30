067801ed7f456a4ea7892d2cf6947a77c24e9a9b
Mehul Pillai <mehulpillai30@gmail.com>
2020-09-29 22:30:44 -0400

    Switch to user profile on preview clic

--- src/components/UserPreviewCard.js
+++ src/components/UserPreviewCard.js
@@ -1,9 +1,10 @@
-import React from 'react'
-import { Card, useTheme, Avatar, makeStyles } from '@material-ui/core';
+import React from "react";
+import { Card, useTheme, Avatar, makeStyles } from "@material-ui/core";
 import AccountCircleIcon from "@material-ui/icons/AccountCircle";
 
-import { getCardStyle, getTextTheme } from '../misc/AppTheme';
-import '../App.css';
+import { getCardStyle, getTextTheme } from "../misc/AppTheme";
+import "../App.css";
+import { useHistory } from "react-router-dom";
 
 const useStyles = makeStyles((theme) => ({
     avatar: {
@@ -20,7 +21,8 @@ const useStyles = makeStyles((theme) => ({
 //profPic: string
 //userName: string
 //bio: string
-export const UserPreviewCard = props => {
+export const UserPreviewCard = (props) => {
+    const history = useHistory();
     const currentTheme = useTheme();
     const cardTheme = getCardStyle(currentTheme);
     const textTheme = getTextTheme(currentTheme);
@@ -28,16 +30,25 @@ export const UserPreviewCard = props => {
     const classes = useStyles();
 
     return (
-        <Card style={{...cardTheme, maxWidth: "100%", width: "400px", margin: "4px"}}>
-            <div  className="col"  style={{paddingLeft: "12px", paddingRight: "12px", paddingTop: "14px"}}>
-                <div className="row" style={{alignItems: "center"}}>
+        <Card
+            style={{ ...cardTheme, maxWidth: "100%", width: "400px", margin: "4px", cursor: "pointer" }}
+            onClick={() => {
+                history.push("/main/profile/" + props.userName);
+            }}
+        >
+            <div className="col" style={{ paddingLeft: "12px", paddingRight: "12px", paddingTop: "14px" }}>
+                <div className="row" style={{ alignItems: "center" }}>
                     <Avatar src={props.profPic} className={classes.avatar}>
                         <AccountCircleIcon className={classes.avIcon} />
                     </Avatar>
-                    <h2 style={{...textTheme, paddingLeft: "20px"}}>{props.userName}</h2>
+                    <h2 style={{ ...textTheme, paddingLeft: "20px" }}>{props.userName}</h2>
                 </div>
-                {props.bio ? <h4 style={{ ...textTheme, paddingLeft: "6px", paddingRight: "6px" }}>{props.bio}</h4> : <i style={{display: "none"}}/>}
+                {props.bio ? (
+                    <h4 style={{ ...textTheme, paddingLeft: "6px", paddingRight: "6px" }}>{props.bio}</h4>
+                ) : (
+                    <i style={{ display: "none" }} />
+                )}
             </div>
         </Card>
     );
-}
+};
