6ca38eb41a46f9e5ed076db8f83d4bf63bbef8bf
Mehul Pillai <mehulpillai30@gmail.com>
2020-09-29 21:58:59 -0400

    Allows gridview for now

--- src/components/SearchUsers.js
+++ src/components/SearchUsers.js
@@ -34,9 +34,9 @@ export const SearchUsers = (props) => {
     }, [page, sort, props.query]);
 
     return (
-        <div className="col" style={{ alignItems: "center" }}>
+        <div className="col" style={{ alignItems: "center", width: "100%" }}>
             <h2 style={textTheme}>Searching for users "{props.query}"</h2>
-            <div className="col" style={{ alignItems: "center" }}>
+            <div className="row" style={{ justifyContent: "center", flexWrap: "wrap", width: "100%"}}>
                 {userList.length ? (
                     userList.map((user) => (
                         <UserPreviewCard
@@ -53,19 +53,10 @@ export const SearchUsers = (props) => {
             {loading ? (
                 <ReactLoading type="bars" color={appThemeConstants.hanPurple} />
             ) : hitMax ? (
-                <i style={{ display: "non" }} />
+                <i style={{ display: "none" }} />
             ) : (
-                <ActionButton label="Load more" width="100px" onClick={() => console.log("load more")} />
+                <ActionButton label="Load more" width="120px" onClick={() => console.log("load more")} />
             )}
         </div>
     );
 };
-
-/*
-<UserPreviewCard
-                key={`user_${user["user_name"]}`}
-                userName={user["user_name"]}
-                profPic={user["prof_pic"]}
-                bio={user["bio"]}
-            />
-*/
