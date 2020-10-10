a0303dcdff14a34e004c6dad797ac4e88be4be6c
Mehul Pillai <mehulpillai30@gmail.com>
2020-10-10 18:42:33 -0400

    Sets up state for gl

--- src/components/GenericList.js
+++ src/components/GenericList.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 //sort: int
 //name: string
@@ -7,5 +7,22 @@ import React from 'react';
 //emptyMessage: string
 //listType: string
 export const GenericList = props => {
+    const [rankedLists, setRankedLists] = useState([]);
+    const [hitMax, setHitMax] = useState(false);
+    const [loading, setLoading] = useState(false);
 
+    useEffect(() => {
+        console.log("rendered")
+    }, []);
+}
+
+function getParams(page = 1, sort = 0, name = "", token = "", query = "", refresh=false, listType) {
+    return {
+        page: page,
+        sort: sort,
+        name: name,
+        token: token,
+        query: query,
+        endpointBase: listType
+    };
 }
