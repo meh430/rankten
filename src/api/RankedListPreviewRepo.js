dec69291fc41a727534c58d4c079542dac6e0887
Mehul Pillai <mehulpillai30@gmail.com>
2020-10-10 18:08:58 -0400

    Sets appropriate endpoint

--- src/api/RankedListPreviewRepo.js
+++ src/api/RankedListPreviewRepo.js
@@ -20,3 +20,34 @@ export const RankedListPreviewTypes = {
         refresh: bool
     } 
 */
+
+export const getRankedListPreview = async (params) => {
+    const { token } = params;
+    if (!token) {
+        token = "";
+    }
+
+    let endpoint = "/" + params.endpointBase;
+    switch (params.endpointBase) {
+        case RankedListPreviewTypes.discoverLists:
+            endpoint += `/${params.page}/${params.sort}`;
+            break;
+        case RankedListPreviewTypes.likedLists:
+            endpoint += `/${params.page}/${params.sort}`;
+            break;
+        case RankedListPreviewTypes.userLists:
+            endpoint += `/${params.name}/${params.page}/${params.sort}`;
+            break;
+        case RankedListPreviewTypes.userListsP:
+            endpoint += `/${params.page}/${params.sort}`;
+            break;
+        case RankedListPreviewTypes.feedLists:
+            endpoint += `/${params.page}`
+            break;
+        case RankedListPreviewTypes.searchLists:
+            endpoint += `/${params.page}/${params.sort}?q=${params.query.replace(/ /g, "+")}`;
+            break;
+    }
+
+    return null;
+}
