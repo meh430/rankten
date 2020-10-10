cda4f2416b1cd9f39eca26fcbcf09b655e3a6f2c
Mehul Pillai <mehulpillai30@gmail.com>
2020-10-10 17:54:56 -0400

    Sets up list preview repo

---
+++ src/api/RankedListPreviewRepo.js
@@ -0,0 +1,8 @@
+export const RankedListPreviewTypes = {
+    discoverLists: "discover",
+    likedLists: "likes",
+    userLists: "rankedlists",
+    userListsP: "rankedlistp",
+    feedLists: "feed",
+    searchLists: "search_lists"
+};
---
+++ src/components/GenericList.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+//sort: int
+//name: string
+//token: string
+//query: string
+//emptyMessage: string
+//listType: string
+export const GenericList = props => {
+
+}
