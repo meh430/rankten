f92208fdd4b8135fe4a957bc748572b4dabcea75
Mehul Pillai <mehulpillai30@gmail.com>
2020-10-12 09:56:38 -0400

    Formats misc

--- src/misc/Utils.js
+++ src/misc/Utils.js
@@ -1,15 +1,27 @@
-const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+];
 
 export const SortOptions = {
     likesDesc: 0,
     dateDesc: 1,
-    dateAsc: 2
+    dateAsc: 2,
 };
 
 export function containsId(idList, id) {
-
     for (var i = 0; i < idList.length; i++) {
-        if (idList[i]['$oid'] === id['$oid']) {
+        if (idList[i]["$oid"] === id["$oid"]) {
             return true;
         }
     }
@@ -19,7 +31,7 @@ export function containsId(idList, id) {
 
 export function tsToDate(timeStamp) {
     const date = new Date(timeStamp);
-    return `${months[date.getMonth()]} ${date.getDate()}, ${date.getFullYear()}`
+    return `${months[date.getMonth()]} ${date.getDate()}, ${date.getFullYear()}`;
 }
 
 export function tsToDelta(timeStamp) {
@@ -34,20 +46,20 @@ export function tsToDelta(timeStamp) {
     const diffInSec = diffInMs / 1000;
 
     if (diffInSec <= 0 || (diffInSec >= 0 && diffInSec < 60)) {
-        return 'Just Now';
+        return "Just Now";
     } else if (diffInMinutes >= 1 && diffInMinutes < 60) {
-        return Math.round(diffInMinutes) + 'm ago';
+        return Math.round(diffInMinutes) + "m ago";
     } else if (diffInHours >= 1 && diffInHours < 24) {
-        return Math.round(diffInHours) + 'h ago';
+        return Math.round(diffInHours) + "h ago";
     } else if (diffInDays >= 1 && diffInDays < 30) {
         if (diffInDays >= 1 && diffInDays < 7) {
-            return Math.round(diffInDays) + 'd ago'
+            return Math.round(diffInDays) + "d ago";
         } else {
-            return Math.round(diffInDays / 7) + 'w ago';
+            return Math.round(diffInDays / 7) + "w ago";
         }
     } else if (diffInDays >= 30 && diffInDays < 365) {
-        return Math.round(diffInDays / 30) + 'mo ago';
+        return Math.round(diffInDays / 30) + "mo ago";
     } else {
-        return Math.round(diffInDays / 365) + 'y ago';
+        return Math.round(diffInDays / 365) + "y ago";
     }
 }
