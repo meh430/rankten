fddafa387c1882fcb221f1be6dbd0b182ded5534
Mehul Pillai <mehulpillai30@gmail.com>
2020-10-26 20:19:46 -0400

    Handles edge case for user preview

--- src/api/UserPreviewRepo.js
+++ src/api/UserPreviewRepo.js
@@ -33,6 +33,11 @@ export async function searchUsers(params) {
     const [e, res] = await api.get("/search_users/" + params.page + "/" + params.sort + "?q=" + params.query);
     if (e) {
         return res.includes("Page") ? [false, true, []] : [e, false, []];
+    } else if (res.length === 100) {
+        const [e1, res1] = await api.get(
+            "/search_users/" + (params.page + 1) + "/" + params.sort + "?q=" + params.query
+        );
+        return res1.includes("Page") ? [false, true, res] : [e1, false, res];
     } else {
         return [e, res.length < 100, res];
     }
