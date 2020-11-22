7266454ec6a3508eb015369b3d50856b3f0d2ae8
Mehul Pillai <mehulpillai30@gmail.com>
2020-11-22 11:56:37 -0500

    Consistency changes

--- src/api/Auth.js
+++ src/api/Auth.js
@@ -4,21 +4,21 @@ import { saveToken } from "../misc/PrefStore";
 export const tokenValid = async(token) => await api.post("/validate_token", token);
 
 export async function loginUser(userName, password) {
-    var [hasError, response] = await api.post("/login", "", { user_name: userName, password: password });
-    if (hasError) {
-        return [hasError, response];
+    var [e, res] = await api.post("/login", "", { user_name: userName, password: password });
+    if (e) {
+        return [e, res];
     } else {
-        saveToken(response["jwt_token"]);
-        return [hasError, response];
+        saveToken(res["jwt_token"]);
+        return [e, res];
     }
 }
 
 export async function signupUser(userName, password, bio) {
-    var [hasError, response] = await api.post("/signup", "", { user_name: userName, password: password, bio: bio });
-    if (hasError) {
-        return [hasError, response];
+    var [e, res] = await api.post("/signup", "", { user_name: userName, password: password, bio: bio });
+    if (e) {
+        return [e, res];
     } else {
-        saveToken(response["jwt_token"]);
-        return [hasError, response];
+        saveToken(res["jwt_token"]);
+        return [e, res];
     }
 }
