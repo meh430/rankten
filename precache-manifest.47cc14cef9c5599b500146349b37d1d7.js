2c376381f30fbbb83948a2071cde537057c02912
Mehul Pillai <mehulpillai30@gmail.com>
2021-01-06 18:54:29 -0500

    Updates

---
+++ precache-manifest.47cc14cef9c5599b500146349b37d1d7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60f512068b28139c22c41b67081d34c2",
+    "url": "/rankten/index.html"
+  },
+  {
+    "revision": "a627f32d3a2d9f3493b6",
+    "url": "/rankten/static/css/main.1f415eea.chunk.css"
+  },
+  {
+    "revision": "e8db3a5bcf1e37003433",
+    "url": "/rankten/static/js/2.c277e0c0.chunk.js"
+  },
+  {
+    "revision": "b47b83f293ef0a67523532a1d2a28ee0",
+    "url": "/rankten/static/js/2.c277e0c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a627f32d3a2d9f3493b6",
+    "url": "/rankten/static/js/main.f40ad684.chunk.js"
+  },
+  {
+    "revision": "39a853ecacb7bc60f390",
+    "url": "/rankten/static/js/runtime-main.1841f665.js"
+  },
+  {
+    "revision": "6a7b3d01c1ee30b39a146aa321ce9915",
+    "url": "/rankten/static/media/ranktenlogo.6a7b3d01.png"
+  }
+]);
